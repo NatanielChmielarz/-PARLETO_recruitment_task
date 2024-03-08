0ad5bcdd5758bbc85e59d1e86f76ac9a3a41d558
Nataniel Chmielarz <129111960+NatanielChmielarz@users.noreply.github.com>
2024-03-08 15:56:05 +0100

    commit

--- project/expenses/forms.py
+++ project/expenses/forms.py
@@ -1,12 +1,26 @@
+import datetime
 from django import forms
-from .models import Expense
+from.models import Expense, Category
 
-
-class ExpenseSearchForm(forms.ModelForm):
-    class Meta:
-        model = Expense
-        fields = ('name',)
+def get_category_list():
+    cat_list = Category.objects.all()
+    choices = [(obj.id, obj.name) for obj in cat_list]
+    return choices
+def get_years_list():
+    years_list = list(range(2000, 2024))
+    return years_list
+class ExpenseSearchForm(forms.Form):
+    name = forms.CharField(required=False)
+    date = forms.DateField(widget=forms.SelectDateWidget(years=get_years_list()))
+    end_date = forms.DateField(widget=forms.SelectDateWidget(years=get_years_list()))
+    category_list = forms.MultipleChoiceField(
+        choices=get_category_list(),
+        widget=forms.CheckboxSelectMultiple(),
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+        self.fields['date'].required = False
+        self.fields['end_date'].required = False
+        self.fields['category_list'].required = False
--- project/expenses/views.py
+++ project/expenses/views.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from django.views.generic.list import ListView
 
 from .forms import ExpenseSearchForm
@@ -15,15 +16,42 @@ class ExpenseListView(ListView):
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
+            date = form.cleaned_data.get('date', '')
+            end_date = form.cleaned_data.get('end_date', '')
+            choices_category_list = form.cleaned_data.get('category_list', [])
+            print('date', date)
+            print('end_date', end_date)
+            print('choices_category_list', choices_category_list)
+           
+            if date and end_date:
+                queryset = queryset.filter(date__range=[date,end_date])
+
+            # # Filter by end_date
+            # if end_date:
+            #     queryset = queryset.filter(date__lte=end_date)
+
+            # Filter by categories
+            if choices_category_list:
+                queryset = queryset.filter(category__in=choices_category_list)
+
+            # Filter by name
             if name:
                 queryset = queryset.filter(name__icontains=name)
+
 
         return super().get_context_data(
             form=form,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
             **kwargs)
+    def clean(self):
+        cleaned_data = super().clean()
 
+        # Check if end_date is greater than or equal to start_date
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        if end_date and start_date and end_date < start_date:
+            raise ValidationError(('End date must be greater than or equal to start date.'))
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
